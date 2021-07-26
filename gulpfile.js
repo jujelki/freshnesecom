59c1bbebf5cabf15236a5b4deef4805ad96f92d4
llallonen <llallonen@gmail.com>
2021-07-26 23:32:41 +0300

    added editorconfig, gitignore, gulpfile and package.json

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const { 
+    watch, 
+    src, 
+    dest,
+    parallel,
+    series
+} = require("gulp");
+const nunjucks = require('gulp-nunjucks');
+const flatten = require('gulp-flatten');
+const del = require('del');
+
+function html(cb) {
+    return src('src/html/*.html')
+    .pipe(nunjucks.compile())
+    .pipe(dest('output/'));
+}
+
+function css() {
+    return src('src/styles/*.css')
+    .pipe(dest('output/'));
+}
+
+const imageTypes = '{jpeg,jpg,svg,png}';
+
+function images() {
+    return src(`src/images/**/*.${imageTypes}`)
+    .pipe(flatten())
+    .pipe(dest('output/'));
+}
+
+function clean() {
+    return del(["output"]);
+}
+
+function watchFiles() {
+    watch("src/html/**/*.html", { ignoreInitial: false }, html);
+    watch("src/styles/**/*.css", { ignoreInitial: false }, css);
+    watch(`src/images/**/*.${imageTypes}`, { ignoreInitial: false }, images);
+}
+
+const build = series(clean, parallel(css, html, images));
+
+exports.watch = parallel(watchFiles);
+exports.clean = clean;
+exports.images = images;
+exports.css = css;
+exports.build = build;
+exports.default = build;
